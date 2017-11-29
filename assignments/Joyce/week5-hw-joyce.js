621f746e97c83ccb6d46ab9ad3bff8ea8588525b
Joyce <joycelee11@users.noreply.github.com>
2017-11-29 19:30:42 +0800

    add week5 hw

---
+++ assignments/Joyce/week5-hw-joyce.js
@@ -0,0 +1,11 @@
+// Question: write a function `randomGroup` which have two arguments `members` and `groupNumber`
+// returns a nested array of members assign in different group randomly, the number of members
+// in each group should evenly distributed as possible.
+
+// For example
+// let members = ['Andy', 'Ben', 'Carol', 'Denny', 'Eric']
+// radnomGroup(members, 2) //return [['Andy', 'Ben', 'Denny'], ['Carol', 'Eric']]
+
+function randomGroup(members, groupNumber){
+  // write code here
+}
