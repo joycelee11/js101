1e900b7a2a7614ce925f7cea53ea577c09d16433
Joyce <joycelee11@gmail.com>
2017-11-29 23:31:34 +0800

    update week5 hw

--- assignments/Joyce/week5-hw-joyce.js
+++ assignments/Joyce/week5-hw-joyce.js
@@ -7,5 +7,37 @@
 // radnomGroup(members, 2) //return [['Andy', 'Ben', 'Denny'], ['Carol', 'Eric']]
 
 function randomGroup(members, groupNumber){
-  // write code here
+	let membersCopy = members.splice(0)
+	let groups = []
+	let memberCount = membersCopy.length
+	let grpMemberCount = Math.floor(membersCopy.length / groupNumber)
+	let bigGrpCount = membersCopy.length % groupNumber
+
+	for (let i = 0; i < groupNumber; i++) {
+		let currentGroup = []
+
+		if (i === (bigGrpCount - 1)) {
+			for (let i = 0; i < grpMemberCount; i++) {
+				let ranNum = Math.floor(Math.random() * (memberCount))
+				currentGroup.push(membersCopy[ranNum])
+				membersCopy.splice(ranNum, 1)
+			}
+		}
+
+		else {
+			for (let i = 0; i < grpMemberCount + 1; i++) {
+				let ranNum = Math.floor(Math.random() * (memberCount))
+				currentGroup.push(membersCopy[ranNum])
+				membersCopy.splice(ranNum, 1)
+			}
+
+		}
+
+		groups.push(currentGroup)
+	}
+
+	return groups
 }
+
+let members = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+console.log(randomGroup(members, 3))
